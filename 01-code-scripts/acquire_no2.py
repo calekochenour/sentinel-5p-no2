dff054573e6468e30c065512c1b51cfe4e80fdc4
Cale Kochenour <cale.kochenour@alumni.psu.edu>
2020-09-16 14:13:00 -0600

    Add script to download NO2 data

---
+++ 01-code-scripts/acquire_no2.py
@@ -0,0 +1,227 @@
+""" Downloads Sentinel-5P NO2 Level-2 processed data (netCDF files)
+
+Run this file in the folder where the data should be downloaded to
+
+Available Product Types:
+Product                   Data file descriptor
+UV Aerosol Index	      AER_AI
+Aerosol Layer Height	  AER_LH
+Carbon Monoxide (CO)	  CO____
+Cloud	                  CLOUD_
+Formaldehyde (HCHO)	      HCHO__
+Methane (CH4)	          CH4___
+Nitrogen Dioxide (NO2)    NO2___
+Sulphur Dioxide (SO2)     SO2___
+Ozone (O3)                O3____
+Tropospheric Ozone 	      O3_TCL
+
+Additional product information here:
+    http://www.tropomi.eu/data-products/level-2-products
+
+Note: Credentials for downloading data (lines 36, 68-69) have been replaced
+      with '########'.
+
+"""
+
+# Imports
+import os
+import json
+import csv
+
+
+def execute_query():
+    """Executes search query for files."""
+    os.system(
+        (
+            f"wget --no-check-certificate --user=######## --password=######## "
+            f"--output-document={query_results_file} "
+            f'"https://s5phub.copernicus.eu/dhus/search?q=beginposition:[{time_span}] '
+            f'AND (footprint:{escape_char}"Intersects({polygon}){escape_char}") '
+            f'AND producttype:L2__{product_type}&rows=100&start=0&format=json"'
+        )
+    )
+
+    return print("Completed query.")
+
+
+def write_to_csv():
+    """Writes the file download links to a CSV file."""
+    # Write UUID links to CSV file
+    with open(
+        f"query_links_{product_type}_{date_range}_{request_number}.csv",
+        "w",
+        newline="",
+    ) as file:
+        csv.writer(file).writerows(uuid_links)
+
+    return print("Completed write to CSV.")
+
+
+def download_files(link_list):
+    """Downloads files based on links."""
+    # Download each file from link
+    for entry in link_list:
+        os.system(
+            (
+                "wget --content-disposition --continue --user=######## "
+                f"--password=######## {entry[0].get('href')}"
+            )
+        )
+
+    return print("Completed download.")
+
+
+def delete_files(file_extension):
+    """Deletes files with a specified extention in the current directory."""
+    # Get files to delete based on extension
+    files_to_delete = [
+        file
+        for file in os.listdir(os.getcwd())
+        if file.endswith(f".{file_extension}")
+    ]
+
+    # Delete files
+    for file in files_to_delete:
+        try:
+            os.remove(file)
+        except Exception as error:
+            print(error)
+
+    return print(f"Completed delete of .{file_extension} files.")
+
+
+# Define Extent
+# Centre County, PA
+# lon_min = -78.4
+# lon_max = -77.1
+# lat_min = 40.6
+# lat_max = 41.3
+
+# South Korea
+# lon_min = 125.0
+# lon_max = 131.0
+# lat_min = 33.1
+# lat_max = 38.7
+
+# North Korea
+# lon_min = 124.1
+# lon_max = 130.7
+# lat_min = 37.6
+# lat_max = 43.1
+
+
+# North and South Korea
+lon_min = 124.1
+lon_max = 131.0
+lat_min = 33.1
+lat_max = 43.1
+
+# Create bounding polygon
+polygon = (
+    "POLYGON(("
+    f"{lon_min} {lat_min},"  # Bottom-Left
+    f"{lon_min} {lat_max},"  # Top-Left
+    f"{lon_max} {lat_max},"  # Top-Right
+    f"{lon_max} {lat_min},"  # Bottom-Right
+    f"{lon_min} {lat_min}))"  # Bottom-Left
+)
+
+# Wuhan bounding box
+# polygon = (
+#     "POLYGON(("
+#     "113.70 30.11,"
+#     "114.90 30.11,"
+#     "114.90 30.97,"
+#     "113.70 30.97,"
+#     "113.70 30.11))"
+# )
+
+# Define time period for downloading data
+# Data availability - 2018-06-28T10:24:07 - 2020-08-06T00:00:00
+time_span = "2020-08-01T00:00:00.000Z TO 2020-08-03T00:00:00.000Z"
+
+# Define all Sentinel-5P products
+sentinel_products = {
+    "UV Aerosol Index": "AER_AI",
+    "Aerosol Layer Height": "AER_LH",
+    "Carbon Monoxide": "CO____",
+    "Cloud": "CLOUD_",
+    "Formaldehyde": "HCHO__",
+    "Methane": "CH4___",
+    "Nitrogen Dioxide": "NO2___",
+    "Sulphur Dioxide": "SO2___",
+    "Ozone": "O3____",
+    "Tropospheric Ozone": "O3_TCL",
+}
+
+# Get product for download
+product_type = sentinel_products.get("Nitrogen Dioxide")
+
+# Define initial query parameters
+request_number = 0
+num_processed_results = 0
+start_date = time_span.split(" ")[0][0:10]
+stop_date = time_span.split(" ")[2][0:10]
+date_range = f"{start_date}_{stop_date}"
+query_results_file = (
+    f"query_results_{product_type}_{date_range}_{request_number}.json"
+)
+escape_char = "\\"
+
+# Execute first query (create JSON file with query results)
+execute_query()
+
+# Create list for file UUID links
+uuid_links = []
+
+# Open JSON
+with open(query_results_file) as file:
+    query_results = json.load(file)
+
+# Get number of results from query (number of files to download)
+num_total_results = int(query_results["feed"]["opensearch:totalResults"])
+
+# Run until all JSON entries have been processed
+while num_processed_results < num_total_results:
+    # Get the number of JSON entries (for download)
+    num_entries = len(query_results["feed"]["entry"])
+    # Loop through each JSON entry
+    for entry in range(0, num_entries):
+        # Extract file title
+        file_title = query_results["feed"]["entry"][entry]["title"]
+        # Check if the file contains NO2 data
+        if "NO2___" in file_title:
+            # Extract file link and UUID
+            file_link = query_results["feed"]["entry"][entry]["link"][0]
+            file_uuid = file_link["href"].split("'")[1]
+            # Append file link and UUID to list
+            uuid_links.append([file_link, file_uuid])
+            # Increment number of processed results
+            num_processed_results += 1
+    # Multiple pages of results - still more files/links to process after
+    #  encountering the maximum allowable per response
+    # Run if the number of query results (total) exceeds the number processed
+    #  (maximum is likely 200 processed, total could be greater)
+    if num_processed_results < num_total_results:
+        # Increment request number
+        request_number += 1
+        # Update name of JSON file with new request number
+        query_results_file = (
+            f"query_results_{product_type}_{date_range}_{request_number}.json"
+        )
+        # Execute another query (create JSON file)
+        execute_query()
+
+        # Open JSON
+        with open(query_results_file) as file:
+            query_results = json.load(file)
+
+# Write query results to CSV
+write_to_csv()
+
+# Download files from links
+download_files(uuid_links)
+
+# Delete CSV and JSON created during acquisition
+for extension in ["json", "csv"]:
+    delete_files(extension)
